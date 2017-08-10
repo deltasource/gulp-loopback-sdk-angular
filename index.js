bccb11133dcd6b5481b365fdc3f67e57916b528c
Daan Kets <daankets@deltasource.eu>
2017-08-10 17:03:54 +0200

    Fix auto-terminate

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
-'use strict';
-const gutil = require('gulp-util');
-const through = require('through2');
-const generator = require('loopback-sdk-angular');
-const path = require('path');
-const WatchJS = require('watchjs');
+"use strict";
+
+const gutil = require("gulp-util");
+const through = require("through2");
+const generator = require("loopback-sdk-angular");
+const path = require("path");
 const Q = require("q");
-
-const watch = WatchJS.watch;
-const unwatch = WatchJS.unwatch;
-
-//  This callback will be passed to watch, this refers to the datasource
-function disconnectDataSource(prop, action, newValue) {
-	if (newValue === true) {
-		this.disconnect();
-		unwatch(this, 'connected');
-	}
-}
 
 module.exports = function(options) {
 	return through.obj(function(file, enc, cb) {
@@ -30,15 +19,15 @@ module.exports = function(options) {
 					let app = require(path.resolve(file.path));
 
 					//  Incase options is undefined.
-					options = options || {ngModuleName: 'lbServices', apiUrl: undefined};
+					options = options || {ngModuleName: "lbServices", apiUrl: undefined};
 
-					options.ngModuleName = options.ngModuleName || 'lbServices';
-					options.apiUrl = options.apiUrl || app.get('restApiRoot') || '/api';
+					options.ngModuleName = options.ngModuleName || "lbServices";
+					options.apiUrl = options.apiUrl || app.get("restApiRoot") || "/api";
 
-					gutil.log('Loaded LoopBack app', gutil.colors.magenta(file.path));
-					gutil.log('Generating',
+					gutil.log("Loaded LoopBack app", gutil.colors.magenta(file.path));
+					gutil.log("Generating",
 						gutil.colors.magenta(options.ngModuleName),
-						'for the API endpoint',
+						"for the API endpoint",
 						gutil.colors.magenta(options.apiUrl)
 					);
 					return app;
@@ -54,7 +43,7 @@ module.exports = function(options) {
 
 						file.contents = new Buffer(script);
 
-						gutil.log('Generated Angular services file.');
+						gutil.log("Generated Angular services file.");
 
 						this.push(file);
 
@@ -62,13 +51,13 @@ module.exports = function(options) {
 						for (var dataSource in dataSources) {
 							if (dataSources.hasOwnProperty(dataSource)) {
 								var ds = dataSources[dataSource];
-								watch(ds, 'connected', disconnectDataSource);
+								ds.disconnect();
 							}
 						}
 					});
 				})
 				.fail((err) => {
-					this.emit('error', new gutil.PluginError('gulp-loopback-sdk-angular', err));
+					this.emit("error", new gutil.PluginError("gulp-loopback-sdk-angular", err));
 				})
 				.nodeify(cb);
 
