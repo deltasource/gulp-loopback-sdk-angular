d362db4b8998599b0f12887fd42485771a378c1f
Daan Kets <daankets@deltasource.eu>
2017-08-10 11:41:15 +0200

    First version

--- index.js
+++ index.js
@@ -1,70 +1,78 @@
 'use strict';
-var gutil = require('gulp-util');
-var through = require('through2');
-var generator = require('loopback-sdk-angular');
-var path = require('path');
-var WatchJS = require('watchjs');
+const gutil = require('gulp-util');
+const through = require('through2');
+const generator = require('loopback-sdk-angular');
+const path = require('path');
+const WatchJS = require('watchjs');
+const Q = require("q");
 
-var watch = WatchJS.watch;
-var unwatch = WatchJS.unwatch;
+const watch = WatchJS.watch;
+const unwatch = WatchJS.unwatch;
 
 //  This callback will be passed to watch, this refers to the datasource
 function disconnectDataSource(prop, action, newValue) {
-  if (newValue === true) {
-    this.disconnect();
-    unwatch(this, 'connected');
-  }
+	if (newValue === true) {
+		this.disconnect();
+		unwatch(this, 'connected');
+	}
 }
 
-module.exports = function (options) {
-  return through.obj(function (file, enc, cb) {
-    if (file.isNull()) {
-      this.push(file);
-      cb();
-      return;
-    }
+module.exports = function(options) {
+	return through.obj(function(file, enc, cb) {
+			if (file.isNull()) {
+				this.push(file);
+				cb();
+				return;
+			}
 
-    var app;
-    try {
-      app = require(path.resolve(file.path));
+			Q
+				.fcall(() => {
+					let app = require(path.resolve(file.path));
 
-      //  Incase options is undefined.
-      options = options || {ngModuleName: 'lbServices', apiUrl: undefined};
+					//  Incase options is undefined.
+					options = options || {ngModuleName: 'lbServices', apiUrl: undefined};
 
-      options.ngModuleName = options.ngModuleName || 'lbServices';
-      options.apiUrl = options.apiUrl || app.get('restApiRoot') || '/api';
+					options.ngModuleName = options.ngModuleName || 'lbServices';
+					options.apiUrl = options.apiUrl || app.get('restApiRoot') || '/api';
 
-      gutil.log('Loaded LoopBack app', gutil.colors.magenta(file.path));
-      gutil.log('Generating',
-        gutil.colors.magenta(options.ngModuleName),
-        'for the API endpoint',
-        gutil.colors.magenta(options.apiUrl)
-      );
+					gutil.log('Loaded LoopBack app', gutil.colors.magenta(file.path));
+					gutil.log('Generating',
+						gutil.colors.magenta(options.ngModuleName),
+						'for the API endpoint',
+						gutil.colors.magenta(options.apiUrl)
+					);
+					return app;
 
-      var script = generator.services(
-        app,
-        options.ngModuleName,
-        options.apiUrl
-      );
+				})
+				.then((app) => {
+					return app.$promise.then(() => {
+						var script = generator.services(
+							app,
+							options.ngModuleName,
+							options.apiUrl
+						);
 
-      file.contents = new Buffer(script);
+						file.contents = new Buffer(script);
 
-      gutil.log('Generated Angular services file.');
+						gutil.log('Generated Angular services file.');
 
-      this.push(file);
+						this.push(file);
 
-      var dataSources = app.dataSources;
-      for (var dataSource in dataSources) {
-        if (dataSources.hasOwnProperty(dataSource)) {
-          var ds = dataSources[dataSource];
-          watch(ds, 'connected', disconnectDataSource);
-        }
-      }
-    } catch (err) {
-      this.emit('error', new gutil.PluginError('gulp-loopback-sdk-angular', err));
-    }
+						var dataSources = app.dataSources;
+						for (var dataSource in dataSources) {
+							if (dataSources.hasOwnProperty(dataSource)) {
+								var ds = dataSources[dataSource];
+								watch(ds, 'connected', disconnectDataSource);
+							}
+						}
+					});
+				})
+				.fail((err) => {
+					this.emit('error', new gutil.PluginError('gulp-loopback-sdk-angular', err));
+				})
+				.nodeify(cb);
 
-    cb();
-    return;
-  });
+
+		}
+	);
 };
